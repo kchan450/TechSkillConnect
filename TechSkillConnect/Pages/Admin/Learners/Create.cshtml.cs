36be954c6107b6e77ad367df237b1271022de9fd
carol chan <thisiscocochan@gmail.com>
2025-04-27 18:07:24 -0700

    learner try

--- TechSkillConnect/Pages/Admin/Learners/Create.cshtml.cs
+++ TechSkillConnect/Pages/Admin/Learners/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -13,19 +14,25 @@ namespace TechSkillConnect.Pages.Admin.Learners
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public CreateModel(ApplicationDbContext context)
+        public CreateModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-            return Page();
+            _userManager = userManager;
         }
 
         [BindProperty]
         public Learner Learner { get; set; } = default!;
+
+        public IActionResult OnGet()
+        {
+            Learner = new Learner
+            {
+                Learner_registration_date = DateTime.UtcNow
+            };
+            return Page();
+        }
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -34,13 +41,35 @@ namespace TechSkillConnect.Pages.Admin.Learners
                 return Page();
             }
 
-            // ✅ Ensure registration date is set automatically
-            Learner.Learner_registration_date = DateTime.UtcNow;
+            // 1. Create a user account in AspNetUsers
+            var user = new IdentityUser
+            {
+                UserName = Learner.LearnerEmail,  // Email as username
+                Email = Learner.LearnerEmail
+            };
 
-            await _context.Learners.AddAsync(Learner);
-            await _context.SaveChangesAsync();
+            var result = await _userManager.CreateAsync(user, "ABC123!");  // Set default password
 
-            return RedirectToPage("./Index");
+            if (result.Succeeded)
+            {
+                // 2. Add learner profile, linked to the AspNetUsers.Id
+                Learner.UserID = user.Id;  // Link to AspNetUsers
+                Learner.Learner_registration_date = DateTime.UtcNow;
+
+                _context.Learners.Add(Learner);
+                await _context.SaveChangesAsync();
+
+                return RedirectToPage("./Index");
+            }
+            else
+            {
+                // Handle user creation errors
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return Page();
+            }
         }
     }
 }
