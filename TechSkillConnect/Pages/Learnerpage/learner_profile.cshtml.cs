a99ec563a2192f641786a18bd853971be1b8bc8d
carol chan <thisiscocochan@gmail.com>
2025-05-02 01:06:23 -0700

    country

--- TechSkillConnect/Pages/Learnerpage/learner_profile.cshtml.cs
+++ TechSkillConnect/Pages/Learnerpage/learner_profile.cshtml.cs
@@ -8,66 +8,55 @@ using TechSkillConnect.Models;
 
 namespace TechSkillConnect.Pages.Learnerpage
 {
-
     [Authorize(Roles = "Learner")]
-    public class LearnerProfilePageModel : PageModel
+    public class learner_profileModel : PageModel
     {
         private readonly ApplicationDbContext _context;
-        public LearnerProfilePageModel(ApplicationDbContext context)
+
+        public learner_profileModel(ApplicationDbContext context)
         {
             _context = context;
         }
-        [BindProperty]
-        public Learner Learner { get; set; }
 
         [BindProperty]
-        public LearnerProfileViewModel LearnerProfile { get; set; }
+        public Learner Learner { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            // Ensure the user has a Tutor record
             Learner = await _context.Learners.FirstOrDefaultAsync(t => t.UserID == userId);
 
             if (Learner == null)
             {
-                // If no tutor record, redirect to the profile creation page
                 return RedirectToPage("/Learnerpage/LearnerProfileCreate");
             }
 
             return Page();
         }
 
-
         public async Task<IActionResult> OnPostAsync()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            ModelState.Remove("UserID");
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            // Check if the TutorProfile exists or if it's a new profile
-            var learnerInfo = await _context.Learners.FirstOrDefaultAsync(p => p.UserID == userId);
+            var learnerInDb = await _context.Learners.FirstOrDefaultAsync(p => p.UserID == userId);
 
-            if (learnerInfo != null)
+            if (learnerInDb != null)
             {
-                // Update the existing profile
-                learnerInfo.Learner_firstname = Learner.Learner_firstname;
-                learnerInfo.Learner_lastname = Learner.Learner_lastname;
-                learnerInfo.CountryOfBirth = Learner.CountryOfBirth;
+                learnerInDb.Learner_firstname = Learner.Learner_firstname;
+                learnerInDb.Learner_lastname = Learner.Learner_lastname;
+                learnerInDb.LearnerEmail = Learner.LearnerEmail;
+                learnerInDb.CountryOfBirth = Learner.CountryOfBirth;
 
+                await _context.SaveChangesAsync();
             }
 
-            // Save the changes to the database
-            await _context.SaveChangesAsync();
-
-            // Redirect to the Tutor Profile page after successful update
             return RedirectToPage("/Learnerpage/learner_dashboard");
         }
-
     }
 }
